7d8fa145c41ca80af8dc5ebdb583bcc24cd821fe
xuandat7 <xuandattran2003@example.com>
2024-05-08 14:58:55 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php 
+
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: index.php");
+						die;
+					}
+				}
+			}
+			
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Reeceflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
+    <body>
+        
+        <div class="signInContainer">
+
+            <div class="column">
+
+                <div class="header">
+                    <img src="assets/images/logo.png" title="Logo" alt="Site logo" />
+                    <h3>Sign In</h3>
+                    <span>to continue to Reeceflix</span>
+                </div>
+
+				<form method="post">
+		
+
+		<input id="text" type="text" name="user_name"><br><br>
+		<input id="text" type="password" name="password"><br><br>
+
+		<input id="button" type="submit" value="Login"><br><br>
+
+		<a href="signup.php">Click to Signup</a><br><br>
+	</form>
+                <a href="register.php" class="signInMessage">Need an account? Sign up here!</a>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
